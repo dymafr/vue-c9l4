1f8b5f4b6e5d357dce244fb9016ee529f5de0338
dymafr <erwan1309@hotmail.com>
2022-03-07 19:05:00 +0100

    2 files modified, 1 added

--- src/Enfant.vue
+++ src/Enfant.vue
@@ -1,12 +1,27 @@
 <template>
-  <h1>Hello</h1>
-  <h2 v-bind="$attrs">Hello 2</h2>
+  <div class="container">
+    <div class="title">
+      <slot name="header"></slot>
+    </div>
+    <section>
+      <slot name="default"></slot>
+    </section>
+    <div class="cta">
+      <slot name="cta"></slot>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
 .container {
-  background-color: red;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 20px;
+  width: 500px;
+  :slotted(h1) {
+    color: gray;
+  }
 }
 </style>
